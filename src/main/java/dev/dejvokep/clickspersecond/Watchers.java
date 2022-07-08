6ef24f9186bc3917deb09793bf633ba2c2e1dd29
dejvokep <davidcubesvkdev@gmail.com>
2022-07-08 23:23:03 +0200

    Added additional code to effectively retrieve watchers.

--- src/main/java/dev/dejvokep/clickspersecond/Watchers.java
+++ src/main/java/dev/dejvokep/clickspersecond/Watchers.java
@@ -7,20 +7,28 @@ import java.util.Map;
 
 public class Watchers {
 
-    private final Map<Player, Player> watchers = new HashMap<>();
+    private final Map<Player, Player> watchers = new HashMap<>(), watched = new HashMap<>();
 
     public Player start(Player watcher, Player watched) {
         Player previous = watchers.remove(watcher);
-        watchers.put(watcher, watched);
+        this.watchers.put(watcher, watched);
+        this.watched.put(watched, watcher);
         return previous;
     }
 
     public Player stop(Player watcher) {
-        return watchers.remove(watcher);
+        Player watched = watchers.remove(watcher);
+        if (watched != null)
+            this.watched.remove(watched);
+        return watched;
     }
 
     public Player getWatched(Player watcher) {
         return watchers.get(watcher);
+    }
+
+    public Player getWatcher(Player watched) {
+        return this.watched.get(watched);
     }
 
 }
