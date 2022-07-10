0cfa67d7aa1415cc5791678091cd9aa7250af2e0
dejvokep <davidcubesvkdev@gmail.com>
2022-07-10 22:29:47 +0200

    Documented and fixed exceptions.

--- src/main/java/dev/dejvokep/clickspersecond/utils/player/UUIDFactory.java
+++ src/main/java/dev/dejvokep/clickspersecond/utils/player/UUIDFactory.java
@@ -8,6 +8,9 @@ import org.jetbrains.annotations.Nullable;
 import java.util.UUID;
 import java.util.regex.Pattern;
 
+/**
+ * A utility class for UUID parsing.
+ */
 public class UUIDFactory {
 
     /**
@@ -15,11 +18,31 @@ public class UUIDFactory {
      */
     private static final Pattern UUID_DASH_PATTERN = Pattern.compile("(\\w{8})(\\w{4})(\\w{4})(\\w{4})(\\w{12})");
 
+    /**
+     * Parses a {@link UUID unique ID} from the given string. If it cannot be parsed (is invalid, or the string is not
+     * exactly 32 or 36 characters long), returns <code>null</code>.
+     *
+     * @param uuid the ID to parse
+     * @return the parsed UUID
+     */
     @Nullable
     public static UUID fromString(@NotNull String uuid) {
-        return uuid.length() == 36 ? UUID.fromString(uuid) : uuid.length() == 32 ? UUID.fromString(UUID_DASH_PATTERN.matcher(uuid).replaceAll("$1-$2-$3-$4-$5")) : null;
+        try {
+            return uuid.length() == 36 ? UUID.fromString(uuid) : uuid.length() == 32 ? UUID.fromString(UUID_DASH_PATTERN.matcher(uuid).replaceAll("$1-$2-$3-$4-$5")) : null;
+        } catch (IllegalArgumentException ex) {
+            return null;
+        }
     }
 
+    /**
+     * Parses a {@link UUID unique ID} from the given command argument. This method not only accepts IDs as described by
+     * {@link #fromString(String)}, but also player names, which it attempts to convert to IDs.
+     * <p>
+     * If the string is not a valid ID and a player could not be found by the name, returns <code>null</code>.
+     *
+     * @param argument the argument to parse
+     * @return the parsed UUID
+     */
     @Nullable
     public static UUID fromArgument(@NotNull String argument) {
         // If a UUID
