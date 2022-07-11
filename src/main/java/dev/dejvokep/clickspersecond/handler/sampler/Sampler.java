91f21e8d9c678b8c49059f104e054cfb8bdd50b2
dejvokep <davidcubesvkdev@gmail.com>
2022-07-11 19:30:00 +0200

    Renamed a method.

--- src/main/java/dev/dejvokep/clickspersecond/handler/sampler/Sampler.java
+++ src/main/java/dev/dejvokep/clickspersecond/handler/sampler/Sampler.java
@@ -39,7 +39,7 @@ public abstract class Sampler {
     /**
      * Resets the {@link #getInfo() cached info} and all CPS counters.
      */
-    public void wipe() {
+    public void wipeData() {
         info = PlayerInfo.empty(info.getUniqueId());
         resetCPS();
     }
